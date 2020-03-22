65ecc80c9d898bcb3383b32efbf557175239651b
Isaac Adams <iadams@emoneyadvisor.com>
2020-03-22 16:51:16 -0400

    init

---
+++ index.ts
@@ -0,0 +1,32 @@
+declare global {
+    interface Array<T> {
+        take(amountToTake: number): T[];
+        takeFromEnd(amountToTake: number): Array<T>;
+        takeRandomly(amountToTake: number): Array<T>;
+    }
+}
+
+Array.prototype.take = function(amountToTake) {
+    return this.splice(0, amountToTake);
+};
+
+Array.prototype.takeFromEnd = function (amountToTake) {
+    return this.splice(this.length - amountToTake, amountToTake);
+};
+
+Array.prototype.takeRandomly = function(amountToTake) {
+    let itemsRemoved: any[] = [];
+
+    while(itemsRemoved.length < amountToTake) {
+        let indexToGetItem = getRandomInt(0, this.length - 1);
+        itemsRemoved = itemsRemoved.concat(this.splice(indexToGetItem, 1));
+    }
+
+    return itemsRemoved;
+};
+
+export function getRandomInt(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
