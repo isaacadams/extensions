85393023ce4c78c939a1b34bb4afec49c235b7c0
Isaac Adams <isaac.d.adams@gmail.com>
2020-04-11 16:48:23 -0400

    adding try catch

--- src/fs.ts
+++ src/fs.ts
@@ -5,11 +5,16 @@ declare module 'fs' {
 }
 
 fs.ensureDirectoryExists = function(path: string, root: string = ''): boolean {
-    path = path.replace(/\\/g, "/");
-    let directories: string[] = path.split('/');
-    let nextDirectory: string | undefined = directories.shift();
-    root = root + nextDirectory + '/';
-    if(!fs.existsSync(root)) fs.mkdirSync(root);
+    try {
+        path = path.replace(/\\/g, "/");
+        let directories: string[] = path.split('/');
+        let nextDirectory: string | undefined = directories.shift();
+        root = root + nextDirectory + '/';
+        if(!fs.existsSync(root)) fs.mkdirSync(root);
 
-    return !directories.length || fs.ensureDirectoryExists(directories.join('/'), root);
+        return !directories.length || fs.ensureDirectoryExists(directories.join('/'), root);
+    }
+    catch {
+        return false;
+    }
 }
