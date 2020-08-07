49e3919c9d3b797087be01aa0d17265661dd98ff
Isaac Adams <isaac.d.adams@gmail.com>
2020-08-07 08:21:46 -0400

    debugging test server

--- test/fs.js
+++ test/fs.js
@@ -30,7 +30,7 @@ describe('fs extension functions', () => {
     it('should create all directories without a root', () => {
         let directoryToCreate = path.join(temp, '/jingle/bells');
         let result = fs.ensureDirectoryExists(directoryToCreate);
-
+        console.log(result);
         expect(fs.existsSync(directoryToCreate)).be.true;
         expect(result).to.not.be.null;
     });
